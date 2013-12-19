6a1e08fb20870247e354f346475836848aad615a
Jacob <kubante@gmail.com>
2013-12-19 01:11:43 +0100

    no message

--- wv/themes/roots-wv/assets/js/plugins/wv-custom-scripts.js
+++ wv/themes/roots-wv/assets/js/plugins/wv-custom-scripts.js
@@ -40,55 +40,15 @@ function buildWikipedia(topic, language) {
     jQuery.ajax({
         url: 'http://en.wikipedia.org/w/api.php',
         data:{
-            action:'parse',
-            prop:'text',
-            page:title,
-            format:'json',
-            redirects:''
+            action:'query',
+            list:'search',
+            srsearch:title,
+            format:'json'
         },
         dataType:'jsonp',
         success: function(data){
-            if(typeof data.parse !== 'undefined'){
-                
-                var wikidesc = jQuery("<div>"+data.parse.text['*']+"<div>").children('p');
-                var wikicard = jQuery("<div>"+data.parse.text['*']+"<div>").children('.infobox');
-                
-                wikidesc.find('sup').remove();
-                wikidesc.find('span.IPA').remove();
-                wikidesc.find('span.nowrap').remove();
-                wikidesc.find('.dablink').remove();
-                wikidesc.find('.editsection').remove();
-                wikidesc.find('.magnify').remove();
-                wikidesc.find('.toc').remove();
-                wikidesc.find('.error').remove();
-                wikidesc.find('a[href$="ogg"]').remove();
-                wikidesc.find('a:contains("edit")').remove();
-                
-                var $desc = wikidesc.html();
-                var $card = wikicard.html();
-                
-                if($desc){
-
-                    var $box_desc = jQuery('<p></p>').append($card);
-                    
-                    if ($card){ $box_desc = $box_desc.append("<br><div id='line'></div><br>" );}
-                        
-                        $box_desc = $box_desc.append($desc);
-
-                        
-                        $box_desc = jQuery('<div class="brick" type="wiki" lang="'+language+'" title="'+title+'"></div>').append($box_desc);
-                        $box_desc.prepend('<span class="cross"> ✘ </span>');
-
-                        $container.append($box_desc).packery( 'appended', $box_desc);
-
-						$box_desc.each( makeEachDraggable );
-                                          
-                        furtherAuthor($box_desc, language);
-                               
-                }
-                }
-              else{console.log("Nothing found on Wikipedia");}
-        },
+			console.log(data);
+		},
         error: function (data){
         
                 var $container = jQuery('#packery');
