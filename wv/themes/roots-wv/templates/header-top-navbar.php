ed0d80fe7ca150265ee71ec6dae5254ff1c8131c
Jacob <kubante@gmail.com>
2014-01-17 18:58:14 +0100

    added register / login buttons

--- wv/themes/roots-wv/templates/header-top-navbar.php
+++ wv/themes/roots-wv/templates/header-top-navbar.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-static-top navbar-inverse" role="banner">
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="fluid-container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,30 +7,38 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" id="logo" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
-    <div class="row top-buffer-small">
-	    <div class="col-lg-6 col-md-6 col-sm-6">
+    <div class="row">
+	    <div class="col-lg-5 col-md-5 col-sm-5 top-buffer-small">
+	    	
 	      	<img class="icon" id="wikipedia-icon" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/wikipedia.png">
 	      	<img class="icon" id="youtube-icon" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/youtube.png">
 	      	<img class="icon" id="flickr-icon" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/flickr.png">
 	        <img class="icon" id="gmaps-icon" src="<?php echo get_stylesheet_directory_uri();?>/assets/img/gmaps.png">
-	      	
-	      	<?php if ( is_user_logged_in() ) { 
-	    		$nonce = wp_create_nonce( 'wall' ); ?>
-	    		<?php if ( is_front_page() ) { ?>
-					<button class="btn btn-default"  id="saveWall" onclick="createWall('<?php echo $nonce ?>');" type="button">Save Wall</button>
-				<?php }  ?>
-				<?php if ( is_singular("wall") ) { ?>
-					<button class="btn btn-default"  id="editWall" onclick="editWall('<?php echo $nonce ?>');" type="button">Save Changes</button>
-				<?php }  ?> 	
-			<?php }  ?> 
-	      	
 	    </div>
-	  
-	     
+	     <div class="col-lg-2 col-md-2 col-sm-2">
+	     	<a class="navbar-brand" id="logo" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+	     </div>
+	  	 <div class="col-lg-5 col-md-5 col-sm-5 top-buffer-small">
+	  	    <?php if ( is_user_logged_in() ) { 
+	    	$nonce = wp_create_nonce( 'wall' ); ?>
+
+	    	<?php if ( is_front_page() ) { ?>
+				<button class="btn btn-default pull-right"  id="saveWall" onclick="createWall('<?php echo $nonce ?>');" type="button">Save Wall</button>
+			<?php }  ?>
+
+			<?php if ( is_singular("wall") ) { ?>
+				<button class="btn btn-default pull-right"  id="editWall" onclick="editWall('<?php echo $nonce ?>');" type="button">Save Changes</button>
+			<?php }  ?> 	
+
+			<?php } else {?> 
+				<button class="btn btn-default pull-right" id="editWall" type="button"><a href="/login" >Login</a></button>
+				<button class="btn btn-default pull-right" id="editWall" type="button"><a href="/wp-login.php?action=register" >Register</a></button>
+			<?php }  ?> 	
+	     </div>
     </div>
     </nav>
   </div>
