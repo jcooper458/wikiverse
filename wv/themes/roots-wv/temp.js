812998a75c3f597a104a17b6a298449087c1251e
Jacob <kubante@gmail.com>
2015-07-10 15:05:15 +0200

    added temp file and bootswatch

---
+++ wv/themes/roots-wv/temp.js
@@ -0,0 +1,40 @@
+//Go get the infobox
+/*$.ajax({
+	url: 'http://'+lang+'.wikipedia.org/w/api.php',
+	data:{
+		action:'parse',
+		page: topic,
+		format:'json',
+		prop:'text',
+		disableeditsection: true,
+		disablepp: true,
+		//preview: true,
+		sectionprevue: true,
+		section:0,
+		disabletoc: true,
+		mobileformat:true
+	},
+	dataType:'jsonp',
+	success: function(data){
+
+		var infobox = $(data.parse.text['*']).find('.infobox');
+
+		if (infobox.length){
+
+			infobox.find('.error').remove();
+			infobox.find('.reference').remove();
+			infobox.find('.references').remove();
+			infobox.find('.org').remove();
+			infobox.find('*').css('max-width', '290px');
+			infobox.find('img').unwrap();
+
+			$brick.append(infobox);
+
+		}
+		//enable to create new bricks out of links
+		buildNextTopic($brick, lang);
+		getWikiLanguages(topic, lang, $brick);
+
+		$packeryContainer.packery();
+	}
+});*/
