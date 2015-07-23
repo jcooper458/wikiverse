c5ac723b2fb31e0c15671f66f71ad59e7a0a8ad2
Jacob <kubante@gmail.com>
2015-07-23 07:06:50 -0400

    removed cpt slug from URL

--- wv/themes/roots-wv/lib/custom.php
+++ wv/themes/roots-wv/lib/custom.php
@@ -3,6 +3,49 @@
  * Custom functions
  */
 /*------------- DISABLE ADMIN BAR -----------------*/
+
+/**
+ * Remove the slug from published post permalinks. Only affect our CPT though.
+ */
+
+function wv_remove_cpt_slug( $post_link, $post, $leavename ) {
+ 
+    if ( 'board' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+ 
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+ 
+    return $post_link;
+}
+add_filter( 'post_type_link', 'wv_remove_cpt_slug', 10, 3 );
+
+
+/**
+ * Have WordPress match postname to any of our public post types (page, post, race)
+ * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
+ * By default, core only accounts for posts and pages where the slug is /post-name/
+ */
+function wv_parse_request_trick( $query ) {
+ 
+    // Only noop the main query
+    if ( ! $query->is_main_query() )
+        return;
+ 
+    // Only noop our very specific rewrite rule match
+    if ( 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+ 
+    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'board', 'page' ) );
+    }
+}
+add_action( 'pre_get_posts', 'wv_parse_request_trick' );
+
+
+//remove the admin bar
 add_filter('show_admin_bar', '__return_false');
 
 /*-------------CREATE CPT board-----------------*/
