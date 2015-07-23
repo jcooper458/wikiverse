efb15d3f43eaa00b492e3d1f0704326144c4bfc4
Jacob <kubante@gmail.com>
2015-07-23 22:52:49 +0200

    added radius to make spiral

--- wv/themes/roots-wv/lib/scripts.php
+++ wv/themes/roots-wv/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dafc217d45e426e2813657ce069280a5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9f71d77e8d9fe4bc719b250e4b859782');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
