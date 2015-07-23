110203134899ecfa343a1dfbc06be08417724003
Jacob <kubante@gmail.com>
2015-07-23 22:56:47 +0200

    Revert "added colors like in the fibonacci clock"
    This reverts commit 90c7209dd7570b5da1f8b94e982aef21a0981d5a.

--- wv/themes/roots-wv/lib/scripts.php
+++ wv/themes/roots-wv/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
 
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '659d3cfe929c1ce60eca786dda313b39');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dafc217d45e426e2813657ce069280a5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
