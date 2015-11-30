4d7a5b6bc3df266d729094bd6f99cd6d2cdc3878
kubante <jacob@wikiver.se>
2015-11-30 13:34:29 +0100

    working on homepage

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
 </div>
 
 <div id="video" class="">
-  <video loop muted autoplay poster="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/wv.png" class="">
+  <video muted autoplay poster="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/wv.png" class="">
     <source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/wv.mp4" type="video/mp4"/>
     <source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/wv.ogv" type="video/ogg"/>
     <source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/wv.webm" type="video/webm"/>
@@ -25,15 +25,13 @@
   <h3>Start with one keyword. Get the craziest connections.</h5>
 </div>	-->
 
-<div class="container buffer-bottom" role="document">
-  <h1><a href="/login">Sign up</a>  or <a href="/start">try it out</a> and start building your own boards!</h1>
-  <h3>Enjoy serendipity by creating some crazy connections.</h3>
-  <h3>Or gather thought-out topics into one handpicked selection.</h3>
+<div class="container-fluid dark-blue">
+  <div class="container buffer-bottom" role="document">
+    <h1><a href="/login">Sign up</a>  or <a href="/start">try it out</a> and start building your own boards!</h1>
+    <h3>Create some crazy connections and enjoy serendipity.</h3>
+    <h3>Or gather thought-out topics into one handpicked selection.</h3>
+  </div>
 </div>
-
-<div class="container buffer-bottom" role="document">
-</div>
-
 
 <?php 
 $fotoURL = "";
@@ -41,34 +39,55 @@ $fotoURL = "";
 $query = new WP_Query( array( 'post_type' => 'board', 'cat' => '3' ) );
 
 ?>
-<div class="container buffer-bottom" role="document">
-<h1>Featured boards</h1>
+<div class="container-fluid">
+  <div class="container buffer-bottom" role="document">
+    <h1>Featured boards</h1>
 
-<div id="packerySite" class="packerySite buffer-top">
-
-<?php 
-if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); 
-
-
-  $the_title = get_the_title();
-  $the_permalink = get_the_permalink();
-  $the_content = get_the_content();
-  $the_content_Array = json_decode($the_content);
-
-?>
-  <div class="brick well well-sm visible">
-
-    <?php if(isset($the_content_Array->featured_image)){ echo '<a href="'. $the_permalink . '"><img src="' . $the_content_Array->featured_image . '"></a>'; }?>
-    <?php echo '<a href="'. $the_permalink . '"><h3>' . $the_title . '</h3></a>'; ?>  
+    <div id="packerySite" class="packerySite buffer-top">
 
     <?php 
-  echo '</div>';
-
-  wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>'));
-
-  ?>
-<?php endwhile; endif; ?>
+    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); 
 
 
+      $the_title = get_the_title();
+      $the_permalink = get_the_permalink();
+      $the_content = get_the_content();
+      $the_content_Array = json_decode($the_content);
+
+    ?>
+      <div class="brick well well-sm visible">
+
+        <?php if(isset($the_content_Array->featured_image)){ echo '<a href="'. $the_permalink . '"><img src="' . $the_content_Array->featured_image . '"></a>'; }?>
+        <?php echo '<a href="'. $the_permalink . '"><h3>' . $the_title . '</h3></a>'; ?>  
+
+        <?php 
+      echo '</div>';
+
+      wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>'));
+
+      ?>
+    <?php endwhile; endif; ?>
+
+    </div>
+  </div>
 </div>
+<div class="container-fluid dark-blue">
+  <div class="container buffer-bottom" role="document">
+    <h1>wikiverse is 100% free and open source</h1>
+    <h3>Because knowledge should be free!</h3>
+    <h3>Fork the project on Github and contribute</h3>
+  </div>
 </div>
+
+
+<div class="container-fluid">
+  <div class="container buffer-bottom" role="document">
+    <h1>wikiverse is 100% free and open source</h1>
+    <h3>Because knowledge should be free!</h3>
+    <h3>Fork the project on Github and contribute</h3>
+  </div>
+</div>
+<!--
+<div class="container buffer-bottom" role="document">
+</div>
+-->
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,41 @@
-<footer class="content-info" role="contentinfo">
+<footer class="content-info dark-blue" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <?php //dynamic_sidebar('sidebar-footer'); ?>
+    <div class="col-lg-3 col-md-3 col-sm-3">
+    	<h4>Footer Title</h4>
+    	<ul class="list-group">
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    	</ul>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3">
+    	<h4>Footer Title</h4>
+    	<ul class="list-group">
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    	</ul>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3">
+    	<h4>Footer Title</h4>
+    	<ul class="list-group">
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    	</ul>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3">
+    	<h4>Footer Title</h4>
+    	<ul class="list-group">
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    		<li class="">asdcsad</li>
+    	</ul>
+    </div>
   </div>
 </footer>
